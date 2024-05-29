a8e2b76011896818ba54b8d37ee99b6725c6be24
Jeremy Gao <145064583+WenSimEHRP@users.noreply.github.com>
2024-05-28 20:36:53 -0700

    Add: basic file structure

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# get current time
+TIME=$(date +"%Y/%m/%d %H:%M:%S")
+
+# preprocess tags.txt with gcc
+gcc -E -x c -DCURRENT_TIME="$TIME PT" -o custom_tags.txt tags.txt &&
+    echo "Finished preprocessing tags.txt" &&
+
+echo "Now preprocessing..." &&
 gcc -E -x c -o wins.nml wins.pnml &&
     echo "Finished preprocessing" &&
     echo "Now compiling..." &&
