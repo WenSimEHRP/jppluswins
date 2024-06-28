035d0a2e2156c0b54216d0645a561ecbeb828b9c
Jeremy Gao <145064583+WenSimEHRP@users.noreply.github.com>
2024-06-27 17:13:41 -0700

    temp commit

--- build.sh
+++ build.sh
@@ -4,13 +4,21 @@
 TIME=$(date +"%Y/%m/%d %H:%M:%S")
 
 # preprocess tags.txt with gcc
-gcc -E -x c -DCURRENT_TIME="$TIME PT" -o custom_tags.txt tags.txt &&
+gcc -E -x c -DCURRENT_TIME="$TIME PT" tags.txt > custom_tags.txt &&
     echo "Finished preprocessing tags.txt" &&
 
 echo "Now preprocessing..." &&
-gcc -E -x c -o wins.nml wins.pnml &&
+gcc -E -x c wins.pnml > wins.nml &&
     echo "Finished preprocessing" &&
     echo "Now compiling..." &&
     nml/nmlc wins.nml -o    wins.grf --nml="wins_parsed.nml" &&
     nml/nmlc wins.nml -c -o wins_crop.grf &&
     echo "Finished compiling!"
+
+# if we have an extra copy argument, copy the files to the right place
+if [ "$1" == "copy" ]; then
+    # path variable
+    path="/d/data/documents/openttd/newgrf"
+    cp wins.grf $path &&
+        echo "Copied wins.grf to ${path}"
+fi
