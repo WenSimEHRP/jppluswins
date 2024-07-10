5684df12ce9445810ab16441bad3b8d195f1365a
Jeremy Gao <145064583+WenSimEHRP@users.noreply.github.com>
2024-07-09 17:36:47 -0700

    Update: install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -3,14 +3,6 @@ pip3 install -r requirements.txt &&
     echo "Finished installing dependencies" &&
 echo "Now cloning nml..."
 git clone https://github.com/openttd/nml &&
-    echo "Finished cloning nml" &&
-    echo "Now merging..." &&
     cd nml &&
-    git config user.name "anonymous" &&
-    git config user.email "anonymous@example.com" &&
-    git remote add pr_source https://github.com/glx22/nml &&
-    git fetch pr_source layout_registers &&
-    git fetch pr_source var6B &&
-    git merge -X ours pr_source/layout_registers -m "Merge layout_registers" &&
-    git merge -X ours pr_source/var6B -m "Merge var6B" &&
-    echo "Finished merging"
+    git checkout dcbfdc7a771e6b3033a9e09c656ac3c441f466db &&
+    echo "Finished cloning NML"
