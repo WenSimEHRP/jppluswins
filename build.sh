9d32093e63bfdedf52dcfa3930315e9c85617356
Jeremy Gao <145064583+WenSimEHRP@users.noreply.github.com>
2024-07-12 00:08:53 -0700

    Update: building script

--- build.sh
+++ build.sh
@@ -3,6 +3,9 @@
 # get current time
 TIME=$(date +"%Y/%m/%d %H:%M:%S")
 
+echo "=============== BUILDING WINS ==============="
+echo "Current time: $TIME"
+
 # preprocess tags.txt with gcc
 gcc -E -x c -DCURRENT_TIME="$TIME PT" tags.txt > custom_tags.txt &&
     echo "Finished preprocessing tags.txt" &&
@@ -14,10 +17,19 @@ gcc -E -x c wins.pnml > wins.nml &&
     nml/nmlc wins.nml -o    wins.grf --nml="wins_parsed.nml" &&
     nml/nmlc wins.nml -c -o wins_crop.grf > /dev/null 2>&1 &&
     echo "Finished compiling!"
+    # optimize the grf
+    ../grf-optimizer/target/release/grf-optimizer.exe ./wins_crop.grf wins_crop_optimized.grf &&
+    yagl -d ./wins_crop_optimized.grf > /dev/null 2>&1 &&
+    echo "Finished optimizing!"
     # no decompiling console output
-    yagl -d wins.grf      > /dev/null 2>&1 &&
-    yagl -d wins_crop.grf > /dev/null 2>&1 &&
-    echo "Finished decompiling!"
+    yagl -d wins.grf                    > /dev/null 2>&1 &&
+    yagl -d wins_crop.grf               > /dev/null 2>&1 &&
+    yagl -d wins_crop_optimized.grf     > /dev/null 2>&1 &&
+    echo "Finished yagl decompiling!"
+    grfcodec -d wins.grf                > /dev/null 2>&1 &&
+    grfcodec -d wins_crop.grf           > /dev/null 2>&1 &&
+    grfcodec -d wins_crop_optimized.grf > /dev/null 2>&1 &&
+    echo "Finished grfcodec decompiling!"
     # show size of final grf, translate to KiB
     size=$(stat --printf="%s" wins.grf)
     size=$(($size / 1024))
@@ -26,13 +38,17 @@ gcc -E -x c wins.pnml > wins.nml &&
     size=$(stat --printf="%s" wins_crop.grf)
     size=$(($size / 1024))
     echo "Cropped GRF size: ${size} KiB"
+    # size of optimized grf
+    size=$(stat --printf="%s" wins_crop_optimized.grf)
+    size=$(($size / 1024))
+    echo "Optimized GRF size: ${size} KiB"
 
 # if we have an extra copy argument, copy the files to the right place
 if [ "$1" == "copy" ]; then
     # path variable
     path="/d/data/documents/openttd/newgrf"
-    cp wins.grf $path &&
-        echo "Copied wins.grf to ${path}"
+    cp wins_crop_optimized.grf $path &&
+        echo "Copied wins_crop_optimized.grf to ${path}"
 fi
 
 # if we have an extra "release" argument, pack the files into a tarball
