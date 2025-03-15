12b87b987cc4616d8170a30883a5af9ed30694e9
Jeremy Gao <wensimehrp@gmail.com>
2025-03-14 21:36:25 -0700

    ......

--- item_template.py
+++ item_template.py
@@ -40,3 +40,77 @@ item(FEAT_STATIONS, i_${name}){
     }
 }
 """)
+
+spritegroup_templates = {}
+spritegroup_templates["default"] = Template(""""
+spriteset(s_cargo_boxes_few, ZOOM_LEVEL_NORMAL, BIT_DEPTH_8BPP) {
+    ${template}(0, 0, "${path}")
+    ${template}(1, 0, "${path}")
+    ${template}(2, 0, "${path}")
+    ${template}(3, 0, "${path}")
+    ${template}(4, 0, "${path}")
+    ${template}(5, 0, "${path}")
+    ${template}(6, 0, "${path}")
+    ${template}(7, 0, "${path}")
+}
+
+spriteset(s_cargo_boxes_little, ZOOM_LEVEL_NORMAL, BIT_DEPTH_8BPP) {
+    ${template}(0, 1, "${path}")
+    ${template}(1, 1, "${path}")
+    ${template}(2, 1, "${path}")
+    ${template}(3, 1, "${path}")
+    ${template}(4, 1, "${path}")
+    ${template}(5, 1, "${path}")
+    ${template}(6, 1, "${path}")
+    ${template}(7, 1, "${path}")
+}
+
+spriteset(s_cargo_boxes_lots, ZOOM_LEVEL_NORMAL, BIT_DEPTH_8BPP) {
+    ${template}(0, 2, "${path}")
+    ${template}(1, 2, "${path}")
+    ${template}(2, 2, "${path}")
+    ${template}(3, 2, "${path}")
+    ${template}(4, 2, "${path}")
+    ${template}(5, 2, "${path}")
+    ${template}(6, 2, "${path}")
+    ${template}(7, 2, "${path}")
+}
+spritegroup sg_${name}_0 {
+    little: [s_none, s_none, s_none, s_none,
+             s_${name}_few, s_${name}_few, s_${name}_few, s_${name}_few, s_${name}_few,
+             s_${name}_little, s_${name}_little, s_${name}_little, s_${name}_little, s_${name}_little,
+    ];
+    lots: [s_${name}_lots];
+}
+spritegroup sg_${name}_1 {
+    little: [s_none, s_none, s_none,
+             s_${name}_few, s_${name}_few, s_${name}_few, s_${name}_few, s_${name}_few,
+             s_${name}_little, s_${name}_little, s_${name}_little, s_${name}_little, s_${name}_little,
+             s_${name}_lots,
+    ];
+    lots: [s_${name}_lots];
+}
+spritegroup sg_${name}_2 {
+    little: [s_none, s_none,
+             s_${name}_few, s_${name}_few, s_${name}_few, s_${name}_few, s_${name}_few,
+             s_${name}_little, s_${name}_little, s_${name}_little, s_${name}_little, s_${name}_little,
+             s_${name}_lots, s_${name}_lots,
+    ];
+    lots: [s_${name}_lots];
+}
+spritegroup sg_${name}_3 {
+    little: [s_none,
+             s_${name}_few, s_${name}_few, s_${name}_few, s_${name}_few, s_${name}_few,
+             s_${name}_little, s_${name}_little, s_${name}_little, s_${name}_little, s_${name}_little,
+             s_${name}_lots, s_${name}_lots, s_${name}_lots,
+    ];
+    lots: [s_${name}_lots];
+}
+
+random_switch(4, TILE, rsw_${name}, bitmask(TRIGGER_STATION_NEW_CARGO, TRIGGER_STATION_NO_MORE_CARGO)) {
+    1: sg_${name}_0;
+    1: sg_${name}_1;
+    1: sg_${name}_2;
+    1: sg_${name}_3;
+}
+""")
